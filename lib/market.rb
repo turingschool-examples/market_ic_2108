a431dc1775b7c5d90919db5f3864cf99a6b7a409
Sam Devine <samuel.will.devine@gmail.com>
2021-09-20 14:55:13 -0600

    Ugghhgh Iteration 3 ugly

--- lib/market.rb
+++ lib/market.rb
@@ -22,4 +22,43 @@ class Market
       vendor.check_stock(item) != 0
     end
   end
+
+  def sorted_item_list
+    items = @vendors.map do |vendor|
+      vendor.inventory.keys.map do |key|
+        key.name
+      end
+    end.flatten.uniq
+
+    items.sort
+  end
+
+  def items_in_market
+    hash = {}
+
+    @vendors.each do |vendor|
+      hash = hash.merge(vendor.inventory) {|key, val1, val2| val1 + val2}
+    end
+    hash
+  end
+
+  def total_inventory
+    hash = {}
+    items = items_in_market
+
+    items.keys.each do |item|
+      if !hash[item]
+        hash[item] = {quantity: items_in_market[item], vendors: vendors_that_sell(item)}
+      else
+        hash[item] += {quantity: items_in_market[item], vendors: vendors_that_sell(item)}
+      end
+    end
+    hash
+  end
+
+  def overstocked_items
+    items_in_market.keys.find_all do |item|
+      items_in_market[item] >= 50 && vendors_that_sell(item).length > 1
+    end
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -60,11 +60,56 @@ RSpec.describe Market do
       expect(@market.vendors_that_sell(@item4)).to eq [@vendor2]
     end
 
-    it '#potential_revenue' do
+    it '#potential_revenue from Vendor class' do
       expect(@vendor1.potential_revenue).to be_a Float
       expect(@vendor1.potential_revenue).to eq 29.75
       expect(@vendor2.potential_revenue).to eq 345.00
       expect(@vendor3.potential_revenue).to eq 48.75
+    end
+  end
+
+  context 'Iteration 3' do
+    before :each do
+      # I am starting to learn about let (instead of before)
+      # and might prefer to use let blocks here instead.
+      # For the sake of time, however, I'm going with before!
+      @market = Market.new('South Pearl Street Farmers Market')
+      @item1 = Item.new({name: 'Peach', price: "$0.75"})
+      @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+      @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      @vendor1 = Vendor.new('Rocky Mountain Fresh')
+      @vendor1.stock(@item1, 35)
+      @vendor1.stock(@item2, 7)
+      @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+      @vendor2.stock(@item4, 50)
+      @vendor2.stock(@item3, 25)
+      @vendor3 = Vendor.new("Palisade Peach Shack")
+      @vendor3.stock(@item1, 65)
+      @vendor3.stock(@item3, 10)
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+    end
+
+    it '#sorted_item_list' do
+      expect(@market.sorted_item_list).to eq [
+        "Banana Nice Cream",
+        "Peach",
+        "Peach-Raspberry Nice Cream",
+        "Tomato"
+      ]
+    end
+
+    it '#total_inventory' do
+      expect(@market.total_inventory).to be_a Hash
+      expect(@market.total_inventory.keys.first).to be_an Item
+      expect(@market.total_inventory.values.first).to be_a Hash
+      expect(@market.total_inventory.values.first.values.first).to eq 100
+    end
+
+    it '#overstocked_items' do
+      expect(@market.overstocked_items).to eq [@item1]
     end
   end
 end
