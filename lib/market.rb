17b447dfd85e932c9b69f3c30c93b7dd0334ab8e
Chaz Simons <chaz.simons@gmail.com>
2021-09-20 12:47:17 -0700

    Add test and method for add_vendor

--- lib/market.rb
+++ lib/market.rb
@@ -5,4 +5,8 @@ class Market
     @name = name
     @vendors = []
   end
+
+  def add_vendor(vendor)
+    @vendors.push(vendor)
+  end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -4,7 +4,14 @@ require './lib/market'
 
 describe Market do
   before(:each) do
-    @market =Market.new("South Pearl Street Farmers Market")
+    @market  = Market.new("South Pearl Street Farmers Market")
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
   end
 
   it 'exists' do
@@ -14,5 +21,19 @@ describe Market do
   it 'has a name and starts with no vendors' do
     expect(@market.name).to eq("South Pearl Street Farmers Market")
     expect(@market.vendors).to eq([])
+  end
+
+  it 'can add vendors and their stock' do
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    expect(@market.vendors).to eq([@vendor1, @vendor2, @vendor3])
   end
 end
