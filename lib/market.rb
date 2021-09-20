fcecfb83704efcbd6018b31799b1d24a91904ba1
Micha Bernhard <mib70@pitt.edu>
2021-09-20 14:53:34 -0600

    Feat - Wrote methods for Market

---
+++ lib/market.rb
@@ -0,0 +1,25 @@
+class Market
+  attr_reader :name,
+              :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map { |vendor| vendor.name }
+  end
+
+  def vendors_that_sell(item)
+    @vendors.find_all do |vendor|
+      if vendor.inventory.keys.include?(item)
+        vendor
+      end
+    end
+  end
+end
