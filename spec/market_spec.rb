4bbc72937b7058fdbcc0219510d7385307f2fc04
oscar santos <oscar303data@gmail.com>
2021-09-20 14:54:45 -0600

    Feat: work on market class

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,1 +1,20 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
 
+RSpec.describe Market do
+  it 'exists' do
+    market = Market.new("South Pearl Street Farmers Market")
+    expect(market).to be_a(Market)
+  end
+
+  it 'has a name' do
+    market = Market.new("South Pearl Street Farmers Market")
+    expect(market.name).to eq("South Pearl Street Farmers Market")
+  end
+
+  it 'does not have vendors yet' do
+    market = Market.new("South Pearl Street Farmers Market")
+    expect(market.vendors).to eq([])
+  end
+end
