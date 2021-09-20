a6d4986a32c1f94c16a9b595f0bea29e731e4984
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-20 15:13:22 -0400

    Create item class

---
+++ spec/item_spec.rb
@@ -0,0 +1,16 @@
+require './lib/item'
+
+RSpec.describe 'Item' do
+  it 'exists' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    expect(item1).to be_an_instance_of(Item)
+  end
+
+  it 'has attributes' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+
+    expect(item1.name).to eq('Peach')
+    expect(item1.price).to eq("$0.75")
+  end
+end
