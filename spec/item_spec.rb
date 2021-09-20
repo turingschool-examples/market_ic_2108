1bda4f8229f0490541923c932a845e009dd6db8b
Gregory Fischer <gwhoisj@gmail.com>
2021-09-20 15:07:50 -0400

    Feat: add item

---
+++ spec/item_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require './lib/item'
+
+describe Item do
+  before :each do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  it 'is an Item' do
+    expect(@item1).to be_a Item
+    expect(@item2).to be_a Item
+  end
+
+  it '#name' do
+    expect(@item2.name).to eq 'Tomato'
+    expect(@item2.price).to eq 0.5
+  end
+end
