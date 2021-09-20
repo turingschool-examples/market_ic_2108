0521276b15785e2edd8d787272c441a292c0558c
Gregory Fischer <gwhoisj@gmail.com>
2021-09-20 15:18:03 -0400

    Iteration 1

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,3 +1,37 @@
 require 'rspec'
 require'./lib/vendor'
 
+describe Vendor do
+  before :each do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  it '#is a vendor' do
+    expect(@vendor).to be_a Vendor
+  end
+
+  it '#name' do
+    expect(@vendor.name).to eq 'Rocky Mountain Fresh'
+  end
+
+  it '#stock' do
+    expect(@vendor.inventory).to eq({})
+    expect(@vendor.check_stock(@item_1)).to eq 0
+  
+    @vendor.stock(@item1, 30)
+
+    expect(@vendor.inventory).to eq({@item1 => 30})
+    expect(@vendor.check_stock(@item1)).to eq 30
+
+    @vendor.stock(@item1, 25)
+
+    expect(@vendor.check_stock(@item1)).to eq 55
+
+    @vendor.stock(@item2, 12)
+
+    expect(@vendor.inventory).to eq({@item1 => 55, @item2 => 12})
+  end
+end
