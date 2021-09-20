cd29eae8a854bcb552f84d4203b7bc76bce12eb2
Lesley Sanders <lesleyasanders@gmail.com>
2021-09-20 16:58:59 -0400

    Final commit

--- lib/market.rb
+++ lib/market.rb
@@ -1,12 +1,24 @@
-class Market
+class Market < Vendor
   attr_reader :name,
-              :vendors
+              :vendors,
+              :items
   def initialize(name)
     @name = name
     @vendors = []
+    @items = []
   end
 
   def add_vendor(name)
-    @vendors << name 
+    @vendors << name
+  end
+
+  def vendor_names
+    vendors.push(@name)
+  end
+
+  def vendors_that_sell(item)
+    @vendors.items.each do |item|
+      @items << item
+    end
   end
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -33,6 +33,55 @@ RSpec.describe 'Market' do
     market.add_vendor(vendor3)
 
     expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+    expect(market.vendor_names).to eq(["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"])
+  end
+
+  it 'has vendors that sell' do
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
+
+    expect(market.vendors_that_sell(item1)).to eq([vendor1, vendor3])
+    expect(market.vendors_that_sell(item4)).to eq([vendor2])
+  end
+
+
+  it 'has potential revenue' do
+    market = Market.new("South Pearl Street Farmers Market")
+    vendor1 = Vendor.new("Rocky Mountain Fresh")
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    vendor1.stock(item1, 35)
+    vendor1.stock(item2, 7)
+
+    vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    vendor2.stock(item4, 50)
+    vendor2.stock(item3, 25)
+
+    vendor3 = Vendor.new("Palisade Peach Shack")
+    vendor3.stock(item1, 65)
+
+    market.add_vendor(vendor1)
+    market.add_vendor(vendor2)
+    market.add_vendor(vendor3)
   end
 
 end
