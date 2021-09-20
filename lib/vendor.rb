daf8ae1ca366e474212229611e2a5da2981a8858
Karan Mehta <karanm645@gmail.com>
2021-09-20 15:57:48 -0500

    Initialize Vendor class

---
+++ lib/vendor.rb
@@ -0,0 +1,23 @@
+class Vendor
+  attr_reader :name,
+              :inventory
+
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
+  end
+
+  def potential_revenue
+    @inventory.sum do |k, v|
+      k.price * v
+    end
+  end
+end
