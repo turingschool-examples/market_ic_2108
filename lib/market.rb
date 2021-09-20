aca763cc5e653805a464365b1b2703263ef7b641
Alex McConnell <alexmmcconnell@gmail.com>
2021-09-20 13:59:28 -0600

    Feat - Add initial market class

---
+++ lib/market.rb
@@ -0,0 +1,23 @@
+class Market
+  attr_reader :name,
+              :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    vendors << vendor
+  end
+
+  def vendor_names
+    vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    vendors.find_all { |vendor| vendor.inventory.include?(item) }
+  end
+end
