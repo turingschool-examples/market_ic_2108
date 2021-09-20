e17e923ee24a089e924d53c47f535f3ab2e04b2c
Mallory Vining <mallorygallery12@gmail.com>
2021-09-20 14:53:38 -0600

    feat: Add final progress

--- lib/market.rb
+++ lib/market.rb
@@ -9,6 +9,27 @@ class Market
   def add_vendor(vendor_names)
     @vendors << vendor_names
   end
+
+  def vendors_that_sell
+    items_in_stock = @vendors.map do |vendor|
+      vendor.stocked
+    end
+    vendors_items = {}
+    items_in_stock.each do |item|
+      item.each do |item_name, item_amount|
+        if !vendors_items[item_name]
+          vendors_items[item_name] = item_amount
+        else
+          vendors_items[item_name] += item_amount
+        end
+      end
+    end
+    vendors_items
+  end
+
+  def potential_revenue
+    @vendors.sum do |vendor| item.potential_revenue
+  end
 end
 
 
